7b0bb13d1ac789229cc7f6100c428d585f2f8032
Ada Eleje <adanna@Adas-MBP.home>
2023-06-28 11:36:03 -0400

    first commit

---
+++ src/components/Banner.js
@@ -0,0 +1,72 @@
+import { useState, useEffect } from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import { ArrowRightCircle } from 'react-bootstrap-icons';
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Banner = () => {
+  const [loopNum, setLoopNum] = useState(0);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [text, setText] = useState('');
+  const [delta, setDelta] = useState(300 - Math.random() * 100);
+  const [index, setIndex] = useState(1);
+  const toRotate = [ "Data Analyst", "Business Analyst", "Web Developer" ];
+  const period = 5000;
+
+  useEffect(() => {
+    let ticker = setInterval(() => {
+      tick();
+    }, delta);
+
+    return () => { clearInterval(ticker) };
+  }, [text])
+
+  const tick = () => {
+    let i = loopNum % toRotate.length;
+    let fullText = toRotate[i];
+    let updatedText = isDeleting ? fullText.substring(0, text.length - 3) : fullText.substring(0, text.length + 3);
+
+    setText(updatedText);
+
+    if (isDeleting) {
+      setDelta(prevDelta => prevDelta / 2);
+    }
+
+    if (!isDeleting && updatedText === fullText) {
+      setIsDeleting(true);
+      setIndex(prevIndex => prevIndex - 1);
+      setDelta(period);
+    } else if (isDeleting && updatedText === '') {
+      setIsDeleting(false);
+      setLoopNum(loopNum + 2);
+      setIndex(1);
+      setDelta(500);
+    } else {
+      setIndex(prevIndex => prevIndex + 1);
+    }
+  }
+
+  return (
+    <section className="banner" id="home">
+      <Container>
+        <Row className="aligh-items-center">
+          <Col xs={12} md={6} xl={7}>
+          <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                <span className="tagline">Welcome to my Portfolio</span>
+                <h1>{`Hi! I'm Ada`} <span className="wrap">{text}</span></h1>
+                  <p>I am a Data and Business Analyst that specializes in data mining, Visualization. I will think of a text to put in because i am not sure what to write.</p>
+                  <button onClick={() => console.log('connect')}>Let’s Connect <ArrowRightCircle size={25} /></button>
+                  </div>}
+            </TrackVisibility>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} alt="Header Img"/>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  )
+}
---
+++ src/components/Contact.js
@@ -0,0 +1,115 @@
+import { useState, useRef} from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import contactImg from "../assets/img/contact-img.svg";
+import emailjs from "@emailjs/browser";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+const Contact = () => {
+    const formRef = useRef();
+    const [form, setForm] = useState({
+      name: "",
+      email: "",
+      message: "",
+    });
+  
+    const [loading, setLoading] = useState(false);
+  
+    const handleChange = (e) => {
+      const { target } = e;
+      const { name, value } = target;
+  
+      setForm({
+        ...form,
+        [name]: value,
+      });
+    };
+  
+    const handleSubmit = (e) => {
+      e.preventDefault();
+      setLoading(true);
+  
+      emailjs
+        .send(
+          'service_oggz234',
+          'template_oc3n7n7',
+          {
+            from_name: form.name,
+            to_name: "Adanna Eleje",
+            from_email: form.email,
+            to_email: "aydeeleje@gmail.com",
+            message: form.message,
+          },
+          'DyaLJmEaMG9mT4czR'
+        )
+        .then(
+          () => {
+            setLoading(false);
+            alert("Thank you. I will get back to you as soon as possible.");
+  
+            setForm({
+              name: "",
+              email: "",
+              message: "",
+            });
+          },
+          (error) => {
+            setLoading(false);
+            console.error(error);
+  
+            alert("Ahh, something went wrong. Please try again.");
+          }
+        );
+    };
+  
+
+    return (
+        <section className="contact" id="connect">
+          <Container>
+            <Row className="align-items-center">
+              <Col size={12} md={6}>
+                <TrackVisibility>
+                  {({ isVisible }) =>
+                    <img className={isVisible ? "animate__animated animate__zoomIn" : ""} src={contactImg} alt="Contact Us"/>
+                  }
+                </TrackVisibility>
+              </Col>
+              <Col size={12} md={6}>
+                <TrackVisibility>
+                  {({ isVisible }) =>
+                    <div className={isVisible ? "animate__animated animate__fadeIn" : ""}>
+                    <h2>Get In Touch</h2>
+                    <form onSubmit={handleSubmit}>
+                      <Row>
+                        <Col size={12} sm={6} className="px-1">
+                          <input type="text" name='name'
+                          value={form.name}
+                          onChange={handleChange}
+                          placeholder="What is your name?"/>
+                        </Col>
+                        <Col size={12} sm={6} className="px-1">
+                          <input type="email" name='email'
+                          value={form.email}
+                          onChange={handleChange}
+                          placeholder="What's your email?" />
+                        </Col>
+                        <Col size={12} className="px-1">
+                          <textarea rows="6" name='message'
+                          value={form.message}
+                          onChange={handleChange}
+                          placeholder='What you want to say?'></textarea>
+                          <button type='submit'>
+                            {loading ? "Sending..." : "Send"} </button>
+                        </Col>
+                      </Row>
+                    </form>
+                  </div>}
+                </TrackVisibility>
+              </Col>
+            </Row>
+          </Container>
+        </section>
+      )
+    }
+
+export default Contact
